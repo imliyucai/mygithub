3cdda3c123038c3ec1e26d5b74c236feae6dc053
imliyucai <yucaili@yeah.net>
2023-04-04 17:10:33 +0800

    Update liy_drv_rtc.h

--- mnLog/liy_drv_rtc.h
+++ mnLog/liy_drv_rtc.h
@@ -25,11 +25,11 @@ typedef struct
 {
   uint8_t hour;
   uint8_t minute;
-	uint8_t second;
-	uint8_t year;
-	uint8_t month;
-	uint8_t day;
-	uint8_t weekday;
+  uint8_t second;
+  uint8_t year;
+  uint8_t month;
+  uint8_t day;
+  uint8_t weekday;
 }  timedate;
 
 //////////////////////////////////////////////////////////////////67
